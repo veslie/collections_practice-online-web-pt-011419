ae8332f1b5ce2f7185beb773709495c70713c387
veslie losius <veslielosius@yahoo.com>
2019-02-10 21:41:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+def number[2,6,8,4,] 
+  number.sort 
+end
 
+
+
+def list_of_names["cat", "fish", "clams", "chicken"]
+  list_of_name.sort(key = len)
+end
+
+def number[1, 2, 3, 4, 5]
+  number.reverse
+end
